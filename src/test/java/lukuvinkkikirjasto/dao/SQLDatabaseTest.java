5ec52bfc4de9aad3f734eb8d41c50ae714f36a22
jmlii <44909874+jmlii@users.noreply.github.com>
2020-12-07 18:00:45 +0200

    Added database tests for search

--- src/test/java/lukuvinkkikirjasto/dao/SQLDatabaseTest.java
+++ src/test/java/lukuvinkkikirjasto/dao/SQLDatabaseTest.java
@@ -1,11 +1,7 @@
-
 package lukuvinkkikirjasto.dao;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import lukuvinkkikirjasto.domain.ReadingTip;
 import org.junit.*;
@@ -14,15 +10,14 @@ import static org.junit.Assert.assertTrue;
 
 
 public class SQLDatabaseTest {
-    
+
     SQLDatabase database;
-    
 
     @Before
     public void setUp() throws SQLException, Exception {
         database = new SQLDatabase("testdatabase.db");
     } 
-    
+
     @After
     public void tearDown() {
         File dbFile = new File("testdatabase.db");
@@ -51,9 +46,9 @@ public class SQLDatabaseTest {
         database.editDescription(id, "Edited description");
         assertEquals("Edited description", database.getTips().get(0).getDescription());
     }
-    
+
     @Test
-    public void readingIsDeletedFromDatabase() throws SQLException {
+    public void readingTipIsDeletedFromDatabase() throws SQLException {
         database.create("Test Item", "Test description");
         ArrayList<ReadingTip> tipList = database.getTips();
         assertEquals(1, tipList.size());
@@ -61,4 +56,36 @@ public class SQLDatabaseTest {
         tipList = database.getTips();
         assertEquals(0, tipList.size());
     }
+    
+    @Test
+    public void readingTipIsFoundWhenSearchedWithCorrectString() throws SQLException {
+        database.create("My title", "My description");
+        database.create("Another Item To Add", "Descriptive text");
+        database.create("One more thing", "Already forgotten");
+        ArrayList<ReadingTip> searchList = database.searchFromTips("add");
+        assertEquals(searchList.get(0).toString(), new ReadingTip(2, "Another Item To Add", "Descriptive text").toString());   
+    }
+    
+    @Test
+    public void readingTipsNotFoundWhenSearchedStringNotInTips() throws SQLException {
+        database.create("My title", "My description");
+        database.create("Another Item To Add", "Descriptive text");
+        database.create("One more thing", "Already forgotten");
+        ArrayList<ReadingTip> searchList = database.searchFromTips("xxx");
+        assertEquals(0, searchList.size());
+    }
+    
+    @Test
+    public void manyReadingTipsFoundWhenSearchedWithCorrectString() throws SQLException {
+        database.create("My title", "My description");
+        database.create("Another Item To Add", "Descriptive text");
+        database.create("One more thing", "Already forgotten");
+        ArrayList<ReadingTip> searchList = database.searchFromTips("descript");
+        assertEquals(2, searchList.size());
+        assertEquals(searchList.get(0).toString(), new ReadingTip(1, "My title", "My description").toString());
+        assertEquals(searchList.get(1).toString(), new ReadingTip(2, "Another Item To Add", "Descriptive text").toString());
+    }
+    
+    
+    
 }
