b899d793fab5f5dca9abd369b39a56f6cf45c17a
jmlii <44909874+jmlii@users.noreply.github.com>
2020-12-07 19:40:06 +0200

    Added ui tests for search

---
+++ src/test/java/lukuvinkkikirjasto/ui/SearchTest.java
@@ -0,0 +1,49 @@
+
+package lukuvinkkikirjasto.ui;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import lukuvinkkikirjasto.domain.ReadingTip;
+import lukuvinkkikirjasto.domain.ReadingTipService;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+
+public class SearchTest {
+    IO io;
+    ReadingTipService rtService;
+    Search search;
+    ArrayList<ReadingTip> searchList;
+
+    @Before
+    public void setUp() throws SQLException {
+        io = mock(IO.class);
+        rtService = mock(ReadingTipService.class);
+        search = new Search(io, rtService);
+        
+        searchList = new ArrayList<>();
+        searchList.add(new ReadingTip(1, "My title", "My description"));
+        when(rtService.searchReadingTips("title")).thenReturn(searchList); 
+
+    }
+    
+    @Test
+    public void searchCallsReadingTipServiceWithRightParameterAndPrints() throws SQLException {
+        when(io.input()).thenReturn("title");
+        search.execute();
+        verify(io).output("Enter search term: ");
+        verify(io, times(1)).input();
+        verify(rtService).searchReadingTips("title");
+        verify(io).output(new ReadingTip(1, "My title", "My description").toString() + "\n");
+    }
+
+    @Test
+    public void searchPrintsRightMessageWhenNoTipsFound() throws SQLException {
+        when(io.input()).thenReturn("xxx");
+        search.execute();
+        verify(rtService).searchReadingTips("xxx");
+        verify(io).output("No tips");
+    }
+    
+}
