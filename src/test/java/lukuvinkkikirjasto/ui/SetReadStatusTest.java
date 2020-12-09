f685d302e3bb89b3db5101cb098e08f69ad718b6
Anton Taleiko <anton.taleiko@gmail.com>
2020-12-09 14:29:30 +0200

    Removed copypaste from MarkAsReadTest and MarkAsUnreadTest by combining them into one refactored test class.

--- src/test/java/lukuvinkkikirjasto/ui/SetReadStatusTest.java
+++ src/test/java/lukuvinkkikirjasto/ui/SetReadStatusTest.java
@@ -9,39 +9,68 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class MarkAsUnreadTest {
+public class SetReadStatusTest {
     
     IO io;
     ReadingTipService rtService;
+    MarkAsRead markAsRead;
     MarkAsUnread markAsUnread;
     
     @Before
     public void setUp() {
         io = mock(IO.class);
         rtService = mock(ReadingTipService.class);
+        markAsRead = new MarkAsRead(io, rtService);
         markAsUnread = new MarkAsUnread(io, rtService);
     }
         
     @Test
+    public void markAsReadWorksWithValidIdAndExistingTip() throws SQLException {
+        setReadStatusWorks(markAsRead, true);
+    }
+
+    @Test
     public void markAsUnreadWorksWithValidIdAndExistingTip() throws SQLException {
+        setReadStatusWorks(markAsUnread, false);
+    }
+
+    private void setReadStatusWorks(Command command, boolean status) throws SQLException {
         when(io.input()).thenReturn("1");
         when(rtService.tipExists(1)).thenReturn(true);
-        markAsUnread.execute();
-        verify(rtService, times(1)).setReadStatus(1, false);
+        command.execute();
+        verify(rtService, times(1)).setReadStatus(1, status);
     }
     
     @Test
+    public void markAsReadGivesErrorMessageWhenIdIsNotANumber() {
+        setReadStatusGivesErrorMessageWhenIdIsNotANumber(markAsRead);
+    }
+
+    @Test
     public void markAsUnreadGivesErrorMessageWhenIdIsNotANumber() {
+        setReadStatusGivesErrorMessageWhenIdIsNotANumber(markAsUnread);
+    }
+
+    private void setReadStatusGivesErrorMessageWhenIdIsNotANumber(Command command) {
         when(io.input()).thenReturn("A");
-        markAsUnread.execute();
+        command.execute();
         verify(io).output("Id must be a number.");
     }
     
     @Test
+    public void markAsReadGivesErrorMessageWhenTipDoesNotExist() throws SQLException {
+        setReadStatusGivesErrorMessageWhenTipDoesNotExist(markAsRead);
+    }
+
+    @Test
     public void markAsUnreadGivesErrorMessageWhenTipDoesNotExist() throws SQLException {
+        setReadStatusGivesErrorMessageWhenTipDoesNotExist(markAsUnread);
+    }
+
+    private void setReadStatusGivesErrorMessageWhenTipDoesNotExist(Command command) throws SQLException {
         when(io.input()).thenReturn("99");
         when(rtService.tipExists(99)).thenReturn(false);
-        markAsUnread.execute();
+        command.execute();
         verify(io).output("Could not find responding tip.");
     }
 }
